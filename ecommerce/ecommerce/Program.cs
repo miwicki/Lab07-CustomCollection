18c58dd1bc34fed610345a1d8dd81f307eaafcda
Matt <wrestler.411@comcast.net>
2018-01-02 11:33:23 -0800

    Starting out, making Product class and ProductType enum

---
+++ ecommerce/ecommerce/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ecommerce
+{
+    class Product
+    {
+        public string ProdName { get; set; }
+        public ProductType Category { get; set; }
+    }
+
+    enum ProductType : int
+    {
+        Electronics = 1,
+        Indoor,
+        Outdoor,
+        Tools,
+        Miscellaneous
+    }
+}
