f4c39971aa829df02810b4b73f5bcb2660498175
Matt <wrestler.411@comcast.net>
2018-01-02 12:38:51 -0800

    added try catch for adding to cart

--- ecommerce/ecommerce/Program.cs
+++ ecommerce/ecommerce/Program.cs
@@ -11,7 +11,37 @@ public static void StoreLoop(Inventory<Product> inventory, Inventory<Product> ca
             Inventory<Product> saveCart = myCart;
             int index = 0;
             int cartIndex = 0;
-            Console.WriteLine("Welcome to Salesman Ricks Multidimensional Internet Emporium!");
+            Console.WriteLine("Welcome to Matt's Online Random Shop");
             Console.WriteLine();
+
+             try
+            {
+                foreach (Product item in inventory)
+                {
+                    Console.WriteLine($"{index}): ITEM: {item.ProductName} - {item.Category}");
+                    index++;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Your Cart:");
+                Console.WriteLine();
+
+                foreach (Product item in cart)
+                {
+                    Console.WriteLine($"{cartIndex}): ITEM: {item.ProductName} - {item.Category}");
+                    cartIndex++;
+                }
+
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                cart.Add(inventory.Remove(input));
+                StoreLoop(inventory, cart);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong; please press enter to retry.");
+                StoreLoop(saveShop, saveCart);
+            }
         }
-    }
+      
+
+        
