5eb5f401e03f9c6f4c30eafc05ba049d4dd189a7
Matt <wrestler.411@comcast.net>
2018-01-02 12:48:42 -0800

    logic for adding to inventory and cart

--- ecommerce/ecommerce/Program.cs
+++ ecommerce/ecommerce/Program.cs
@@ -6,12 +6,18 @@ namespace ecommerce
 public static void StoreLoop(Inventory<Product> inventory, Inventory<Product> cart)
         {
             Console.Clear();
+
+
             Inventory<Product> myCart = new Inventory<Product>();
             Inventory<Product> saveShop = inventory;
             Inventory<Product> saveCart = myCart;
+
+
             int index = 0;
             int cartIndex = 0;
-            Console.WriteLine("Welcome to Matt's Online Random Shop");
+
+
+            Console.WriteLine("Welcome to Matt's Online Random Product Shop");
             Console.WriteLine();
 
              try
@@ -21,15 +27,19 @@ public static void StoreLoop(Inventory<Product> inventory, Inventory<Product> ca
                     Console.WriteLine($"{index}): ITEM: {item.ProductName} - {item.Category}");
                     index++;
                 }
+
+
                 Console.WriteLine();
                 Console.WriteLine("Your Cart:");
                 Console.WriteLine();
+
 
                 foreach (Product item in cart)
                 {
                     Console.WriteLine($"{cartIndex}): ITEM: {item.ProductName} - {item.Category}");
                     cartIndex++;
                 }
+
 
                 int input = Convert.ToInt32(Console.ReadLine());
 
@@ -42,6 +52,7 @@ public static void StoreLoop(Inventory<Product> inventory, Inventory<Product> ca
                 StoreLoop(saveShop, saveCart);
             }
         }
+ }
       
 
         
